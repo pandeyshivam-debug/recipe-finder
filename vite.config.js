439925c524f193bc8b663f7e9e25eaec997e5ce0
pandeyshivam-debug <shivam.pandey@minfytech.com>
2025-07-18 12:33:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   plugins: [
     react(), tailwindcss()
   ],
+  server: {
+    port: 3000,     // Set dev server port
+    host: true      // Allow access from network (e.g., LAN or external IP)
+  }
 })
